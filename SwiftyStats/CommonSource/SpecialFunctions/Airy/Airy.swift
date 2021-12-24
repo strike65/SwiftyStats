120644b65c5913493607530d58df7093adb44182
Volker Thieme <not_my_name+github@mailbox.org>
2021-12-24 11:23:04 +0100

    typos

--- SwiftyStats/CommonSource/SpecialFunctions/Airy/Airy.swift
+++ SwiftyStats/CommonSource/SpecialFunctions/Airy/Airy.swift
@@ -1,5 +1,6 @@
 /*
- Copyright (2017-2019) strike65
+
+Copyright (2017-2019) strike65
  
  GNU GPL 3+
  
@@ -24,11 +25,11 @@ extension SSSpecialFunctions {
     /// - Note:
     /// -- Algorithm 838: Airy Functions. ACM Transactions on Mathematical Software, Vol. 30, No. 4, December 2004.
     ///
-    /// -- FABIJONAS, LOZIER, OLVER: Computation of Complex Airy Functions and Their Zeros Using Asymptotics and the Differential EquationACM Transactions on Mathematical Software, Vol. 30, No. 4, December 2004, Pages 471–490
+    /// -- FABIJONAS, LOZIER, OLVER: Computation of Complex Airy Functions and Their Zeros Using Asymptotics and the Differential Equation. ACM Transactions on Mathematical Software, Vol. 30, No. 4, December 2004, Pages 471–490
     /// - Remark:
     /// Errors (Double)
     ///
-    /// for -200 <= x <= 200, step 0.5
+    /// for Ai: -200 <= x <= 200, step 0.5
     ///
     /// -- min. abs. Error: 0.0, max. abs. Error: 6.153844794853924e-14
     ///
@@ -55,16 +56,13 @@ extension SSSpecialFunctions {
     /// - Remark:
     /// Errors (Double)
     ///
-    /// for -200 <= x <= 2, step: 0.5:
+    /// for Bi: -200 <= x <= 2, step: 0.5:
     ///
     /// -- min. abs. Error: 0.0, max. abs. Error: 5.259681579161679e-14
-    ///
     /// -- rel. Error: 0.0, max. rel. Error: 6.261649779273941e-12
     ///
-    /// for Ai': -200 <= x <= 2:
-    ///
+    /// for Bi': -200 <= x <= 2:
     /// -- min. abs. Error: 0.0, max. abs. Error: 8.637326964766601e-13
-    ///
     /// -- min. rel. Error: 0.0, max. rel. Error: 2.658098530832e-11
     internal static func airyBi<T:SSFloatingPoint>(x: T) -> (bi: T, dbi: T, error: Int) {
         var ans: (bi: T, dbi: T, error: Int)
